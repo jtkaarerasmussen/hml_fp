55b6e86787c6cd363d64b8bf88bb7b5f237ae82f
Jonatan <jtkaarerasmussen@gmail.com>
2025-02-11 20:07:16 -0800

    sp_runs

--- sp_runs.py
+++ sp_runs.py
@@ -1,13 +1,27 @@
 from bootstrap_sp_tool import *
 import sys
+import os
 
-taxis_vals = np.linspace(0,1,30)
+taxis_vals = np.linspace(0,0.3,30)
+mem_vals = np.linspace(0,0.3,30)
 
 f_ic = linspace_circ(1000,1000,5)
 s_ic = linspace_circ(25,25,5)
-m = 0 
-d = 0.1
 
-for t in taxis_vals :
-    save_dir = f"/home/jonatank/Documents/HML/hml_fp/trial_runs/1dtaxis/1dtaxis_{sys.argv[1]}/d_0.1/t_{t}/"
-    sp_run(t,m,d,f_ic,s_ic, save_dir)
+base_dir = os.getcwd()+"/"
+
+for i in range(10):
+    m = 0 
+    d = 0.1
+
+    for t in taxis_vals :
+        save_dir = f"{base_dir}trial_runs/1dtaxis/1dtaxis_{i}/d_0.1_t_{t}/"
+        sp_run(t,m,d,f_ic,s_ic, save_dir)
+
+
+    t = 0 
+    d = 0.1
+
+    for m in mem_vals :
+        save_dir = f"{base_dir}trial_runs/1dmem/1dmem_{i}/d_0.1_m_{m}/"
+        sp_run(t,m,d,f_ic,s_ic, save_dir)
